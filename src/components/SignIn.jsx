cafdf99f008067c5f18a86ade67c7e264525843d
Alujan28 <kovialujan@gmail.com>
2024-10-31 20:18:00 +0530

    feat:add SignIn form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./SignIn.css";
 import logo from "../img/logo.png";
+import { Link } from "react-router-dom";
 
 export default function SignIn() {
     return (
@@ -8,6 +9,19 @@ export default function SignIn() {
         <div>
             <div className="loginForm">
             <img className="signupLogo" src={logo} alt=""/>
+            <div>
+                <input type="email" name="email" id="email" placeholder="Email" />
+            </div>
+            <div>
+                <input type="password" name="password" id="password" placeholder="Password" />
+            </div>
+            <input type="submit" id="login-btn" value="Sign In" />
+            </div>
+            <div className="loginForm2">
+                Don't have an account ?
+                <Link to="/signup">
+                <span style={{ color: "blue", cursor: "pointer"}}>Sign Up</span>
+                </Link>
             </div>
         </div>
     </div>
