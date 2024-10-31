b5da383150cbe27e3d03bdcfacff7798aa426c4c
Alujan28 <kovialujan@gmail.com>
2024-10-31 10:41:34 +0530

    feat: addnav logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../img/logo.png";
+import "./Navbar.css";
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <img src={logo} alt=""/>
+            
+        </div>
+    )
+}
