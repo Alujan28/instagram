207794dce5b6817a588140c92a05604f3c57096c
Alujan28 <kovialujan@gmail.com>
2024-10-31 19:58:08 +0530

    feat:add SignIn page logo

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import "./SignIn.css";
+import logo from "../img/logo.png";
 
 export default function SignIn() {
-    return <div>SignIn</div>;
+    return (
+    <div className="signin">
+        <div>
+            <div className="loginForm">
+            <img className="signupLogo" src={logo} alt=""/>
+            </div>
+        </div>
+    </div>
+
+    );
 }
