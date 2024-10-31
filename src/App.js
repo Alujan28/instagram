f7217f133a0332e03f6260c979c5f4f70baf6f9f
Alujan28 <kovialujan@gmail.com>
2024-10-31 12:56:50 +0530

    feat:add SignUp page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import Profile from "./components/Profile";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-  <div className="App">
+    <BrowserRouter>
+    <div className="App">
     <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
+      <Route path="/signin" element={<SignIn/>}></Route>
+      <Route path="/profile" element={<Profile/>}></Route>
+    </Routes>
     </div>
+    </BrowserRouter>
+  
   );
 }
 
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "../img/logo.png";
+import "./SignUp.css";
+import { Link } from "react-router-dom";
+
+export default function SignUp() {
+    return( 
+    <div className="signup">
+        <div className="form-container">
+            <div className="form">
+            <img className="signupLogo" src={logo} alt=""/>
+            <p className="loginPara">
+                Sign up to see photos and videos <br/> from your friends.    
+            </p>
+            <div>
+                <input type="email" name="email" id="email"
+                 placeholder="Email" />
+            </div>
+            <div>
+                <input type="text" name="name" id="name"
+                 placeholder="Full Name" />
+            </div>
+            <div>
+                <input type="text" name="username" id="username"
+                 placeholder="Username" />
+            </div>
+            <div>
+                <input type="password" name="password" id="password"
+                 placeholder="Password" />
+            </div>
+            <p className="loginPara" style={{fontSize:"12px",margin:"3px 0px" }}>
+                By signing up, you agree to our Terms, <br /> policy and cookies policy.
+            </p>
+            <input type="submit" id="submit-btn" value="Sign Up" />
+            </div>
+            <div className="form2">
+                Already have an account ?
+                <Link to="signin">
+                <span style={{color: "blue", cursor:"pointer"}}>Sign In</span>
+                </Link>
+            </div>
+        </div>
+    </div>
+    );
+}
